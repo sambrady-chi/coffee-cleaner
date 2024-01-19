229a58a5b9d8809c9274a0a654e654ce0d57c0f7
Sam Brady <sambrady@evertrue.com>
2024-01-19 13:08:49 -0600

    fix pathing

--- commands/clean.js
+++ commands/clean.js
@@ -21,7 +21,10 @@ const clean = (coffeePaths) => {
      * @link https://github.com/facebook/jscodeshift/blob/48f5d6d6e5e769639b958f1a955c83c68157a5fa/bin/jscodeshift.js#L18
      */
     const options = {
-      transform: "./js-code-shift-scripts/transformer.js",
+      transform: path.resolve(
+        __dirname,
+        "../js-code-shift-scripts/transformer.js"
+      ),
       verbose: 0,
       dry: false, // mark true if you dont want to actually modify the files, just print the output
       print: false, // set to true to print what your new file looks like
--- js-code-shift-scripts/transformer.js
+++ js-code-shift-scripts/transformer.js
@@ -1,9 +1,12 @@
-const findReact = require("./utils/jscodeshift-helpers");
-const replaceDOMFactories = require("./transforms/react-dom-factories-to-react-create-element");
-const convertCreateElementToJsx = require("./transforms/react-create-element-to-jsx");
-const removeModuleExports = require("./transforms/remove-module-exports");
-const replaceCreateComponentWithCreateClass = require("./transforms/replace-create-component-with-react");
-const replaceReturnStatementWithVariableDeclaration = require("./transforms/replace-return-with-variable-declaration");
+const path = require("path");
+
+const pather = (p) => path.resolve(__dirname, p);
+const findReact = require(pather("./utils/jscodeshift-helpers"));
+const replaceDOMFactories = require(pather("./transforms/react-dom-factories-to-react-create-element"));
+const convertCreateElementToJsx = require(pather("./transforms/react-create-element-to-jsx"));
+const removeModuleExports = require(pather("./transforms/remove-module-exports"));
+const replaceCreateComponentWithCreateClass = require(pather("./transforms/replace-create-component-with-react"));
+const replaceReturnStatementWithVariableDeclaration = require(pather("./transforms/replace-return-with-variable-declaration"));
 
 function transformer(file, api, options) {
   const j = api.jscodeshift;
@@ -16,10 +19,14 @@ function transformer(file, api, options) {
   // Add other modifications here as we want
   if (hasReact) {
     hasModifications = removeModuleExports(j, root);
-    hasModifications = replaceDOMFactories(j, root, hasReact) || hasModifications;
+    hasModifications =
+      replaceDOMFactories(j, root, hasReact) || hasModifications;
     hasModifications = convertCreateElementToJsx(j, root) || hasModifications;
-    hasModifications = replaceCreateComponentWithCreateClass(j, root) || hasModifications;
-    hasModifications = replaceReturnStatementWithVariableDeclaration(j, root) || hasModifications;
+    hasModifications =
+      replaceCreateComponentWithCreateClass(j, root) || hasModifications;
+    hasModifications =
+      replaceReturnStatementWithVariableDeclaration(j, root) ||
+      hasModifications;
   }
   return hasModifications ? root.toSource({ quote: "single" }) : null;
 }
